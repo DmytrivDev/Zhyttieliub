392f1a820a0731c6b3d76432a09d0187f16c3434
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-05-10 15:30:07 +0300

    asd

--- src/js/parts/parallax.js
+++ src/js/parts/parallax.js
@@ -1,5 +1,3 @@
-import simpleParallax from 'simple-parallax-js';
-
 const galSection = document.querySelector('.section__gallery');
 
 export const parallaxFunc = () => {
@@ -8,19 +6,6 @@ export const parallaxFunc = () => {
     const offset = elemRect.top;
     const parVal = offset / 1500 - 0.4;
 
-    console.log(parVal)
-
     document.documentElement.style.setProperty("--parallax", parVal);
   });
-
-  //   const parallax = document.querySelectorAll('.paralaxMove');
-
-  //   const options = {
-  //     scale: 1,
-  //     maxTransition: 100
-  //   }
-
-  //   parallax?.forEach((el) => {
-  //     new simpleParallax(el, options);
-  //   });
 };
