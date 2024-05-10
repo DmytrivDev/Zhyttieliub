46af4c6879f4bdb044b9b5e6d784882454c35a48
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-05-10 15:18:46 +0300

    ghjgh

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
+      outDir: 'public',
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
